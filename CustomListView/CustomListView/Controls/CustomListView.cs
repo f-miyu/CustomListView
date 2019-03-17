49a9e7cd0b3ad588ac977a52b812f6d92bd5897b
松島史幸 <matsushima23@gmail.com>
2019-03-17 20:05:16 +0900

    修正

--- CustomListView/CustomListView/Controls/CustomListView.cs
+++ CustomListView/CustomListView/Controls/CustomListView.cs
@@ -7,5 +7,12 @@ namespace CustomListView.Controls
 {
     public class CustomListView : ListView
     {
+        public CustomListView() : base()
+        {
+        }
+
+        public CustomListView(ListViewCachingStrategy cachingStrategy) : base(cachingStrategy)
+        {
+        }
     }
 }
